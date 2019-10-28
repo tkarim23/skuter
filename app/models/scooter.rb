1076108cc2ecf076853789b15e94883a1d1fa04a
ashna shah <a5hna@hotmail.co.uk>
2019-10-28 18:55:28 +0800

    added the validations to all models

--- app/models/scooter.rb
+++ app/models/scooter.rb
@@ -1,4 +1,15 @@
 class Scooter < ApplicationRecord
   belongs_to :model
   belongs_to :user
+  has_many :bookings
+  has_many :reviews, through: :bookings
+
+  validates :model_id, presence: true
+  validates :user_id, presence: true
+  validates :location, presence: true
+  validates :age, presence: true
+  validates :mileage, presence: true
+  validates :condition, presence: true
+  validates :photo, presence: true
+  validates :price_per_day, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :reviews
+  belongs_to :booking
+  belongs_to :scooter
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :bank_account, presence: true, uniqueness: true
 end
