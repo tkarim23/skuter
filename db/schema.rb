48a0d5bb2e56db1468b4dca6991009c56ce58698
ashna shah <a5hna@hotmail.co.uk>
2019-10-28 19:27:31 +0800

    added colour field to scooters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_091839) do
+ActiveRecord::Schema.define(version: 2019_10_28_112045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_10_28_091839) do
     t.integer "price_per_day"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "colour"
     t.index ["model_id"], name: "index_scooters_on_model_id"
     t.index ["user_id"], name: "index_scooters_on_user_id"
   end
