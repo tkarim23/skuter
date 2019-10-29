2b2447b4ba58211199fc5a641a86fe919487e2c1
Santiago Campos <ycl@outlook.com>
2019-10-29 15:48:44 +0800

    Fixed user editing, added user dashboard code and made navbar functional

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def user_dashboard
+    @user = current_user
+    @listings = Scooter.where(user: @user)
   end
 end
