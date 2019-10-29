50e33ad0ee4bcafd3f3c0588e49efc6716247a5c
Santiago Campos <ycl@outlook.com>
2019-10-29 14:51:21 +0800

    new scooter page done, scooters controller working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,16 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      #redirect to
+    else
+      render :new
   end
 
   def show
@@ -12,8 +20,21 @@ class BookingsController < ApplicationController
   end
 
   def update
+     @booking.update(booking_params)
+    redirect_to action: "show"
   end
 
   def destroy
   end
+
+   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :scooter_id, :start_date, :end_date)
+  end
+
 end
