4d2b8aae93536597ac39f548ae25506b297e245d
ashna shah <a5hna@hotmail.co.uk>
2019-10-29 11:46:36 +0800

    added all the routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  def home
+  def user_dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'models#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :models do
-    resources :scooters
+  resources :models, only: [:index, :show]
+
+
+  resources :scooters, only: [:show, :delete, :edit, :update, :new, :create ] do
+    resources :bookings, only: [:create]
+end
+
+  resources :bookings, only [:show] do
+    resources :reviews, only [:create]
   end
 
-
+  get "user_dashboard", to: "pages#user_dashboard"
 end
