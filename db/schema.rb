f638e4090d26531c0c2fe43771a203d4ea702726
Santiago Campos <ycl@outlook.com>
2019-10-30 15:33:32 +0800

    added autocomplete, some geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_105757) do
+ActiveRecord::Schema.define(version: 2019_10_30_063904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_105757) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "colour"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["model_id"], name: "index_scooters_on_model_id"
     t.index ["user_id"], name: "index_scooters_on_user_id"
   end
